e3b2b5468c21103c38b1c88b4e024008c4571ebb
Zee-2000 <104075923+Zee-2000@users.noreply.github.com>
2024-02-11 18:41:37 +0000

    Inherited

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" defines inherited class form list"""""
+""" defines inherited class form list"""
 
 class MyList(list):
-  """ implements a sorted printing for class list"""
-  def prjnt_sorted(self):
-      """" prjnt list of sorted as sec ending """"
-    print(sorted(self))
+    """ implements a sorted printing for class list"""
+    def print_sorted(self):
+        """ print list of sorted assecending """
+        print(sorted(self))
