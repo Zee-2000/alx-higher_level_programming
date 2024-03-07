a07ce04c1e5b1244852f07b046e58142bf7a2e1e
Zee-2000 <zahwaabdelnasser@gmail.com>
2024-03-07 19:05:56 +0200

    input/output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+
+"""This module defines json"""
+
+def to_json_string(my_obj):
+    """this function returns json representation to string"""
+    return json.dumps(my_obj)
