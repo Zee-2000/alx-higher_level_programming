a691e177ec371272341e30546ce22b4fd22c7c9d
Zahwa A. El Nasser <104075923+Zee-2000@users.noreply.github.com>
2024-09-25 17:53:50 +0300

    Create 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- SQL script that creates the database hbtn_0d_2 and the user user_0d_2. 
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
